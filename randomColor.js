9edf7a9c644c061e8ffe385c4983730483351e7e
Mykhailo Bondarenko <79281448+miha-bond@users.noreply.github.com>
2022-11-09 00:43:54 +0200

    Update randomColor.js

--- randomColor.js
+++ randomColor.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+АБО
+
+function generateRandomColor() {
+  let color = '#';
+  for (let i = 0; i < 3; i++)
+    color += (
+      '0' + Math.floor((Math.random() * Math.pow(16, 2)) / 2).toString(16)
+    ).slice(-2);
+  return color;
+}
